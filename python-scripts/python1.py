a4e569a789435e9ece1fb2678ec6dfff068254d2
Anna Cunningham <Anna@DN52eggv.SUNet>
2016-10-04 15:38:53 -0700

    Edit file path in doctest

--- python-scripts/python1.py
+++ python-scripts/python1.py
@@ -1,9 +1,23 @@
 import pandas
 
+
 def read_file_1(filename):
     """ Reads in a file
-    >>> read_file_1('../testfiles/testfile1.txt')
+    >>> read_file_1('testfiles/testfile1.txt')
     'hello'
     """
     with open(filename) as F:
         return F.read().strip()
+
+from argparse import ArgumentParser
+
+def prepare_parser():
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input', dest='input', type=str,
+                        required=True, help='input file')
+    return parser
+
+if __name__ == "__main__":
+    options = prepare_parser().parse_args()
+
+    print read_file_1(options.input)
